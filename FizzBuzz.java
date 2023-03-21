e43cf279b4e8b25c1368fd080e83afec80b43531
Ceffrece <cheffrece@quinnipiac.edu>
2023-03-21 19:40:46 -0400

    added fizzbuzz program

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,5 +1,22 @@
 public class FizzBuzz {
     public static void main(String[] args){
-        
+        for(int count = 1; count <= 100; count++){
+            if(count%3 == 0){
+                if(count%5 == 0){
+                    System.out.println("FizzBuzz");
+                }
+                else{
+                    System.out.println("Fizz");
+                }
+            }
+            else{
+                if(count%5 == 0){
+                    System.out.println("Buzz");
+                }
+                else{
+                    System.out.println(count);
+                }
+            }
+        }
     }
 }
